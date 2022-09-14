923887140b286077dc87fec4a1f4fce66791b745
Battal98 <battal-patlar@hotmail.com>
2022-09-14 16:35:09 +0300

    particle bullet demo and enemy target pos are orginazed.

---
+++ Assets/Scripts/Controllers/ParticleTrigger.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTrigger : MonoBehaviour
+{
+
+    private void OnParticleCollision(GameObject other)
+    {
+            Debug.Log("hit");
+
+    }
+}
--- Assets/Scripts/Controllers/Player/PlayerPhysicController.cs
+++ Assets/Scripts/Controllers/Player/PlayerPhysicController.cs
@@ -1,25 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Enums;
 public class PlayerPhysicController : MonoBehaviour
 {
+    [SerializeField]
+    private PlayerLayerType playerLayerType;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("GateExit"))
+        if (other.CompareTag("Gate"))
         {
-            gameObject.layer = LayerMask.NameToLayer("BattleYard");
-        }
 
-        if (other.CompareTag("GateEntry"))
-        {
-            gameObject.layer = LayerMask.NameToLayer("Base");
+            if (other.transform.position.z > transform.position.z)
+            {
+                playerLayerType = PlayerLayerType.Base;
+                gameObject.layer = LayerMask.NameToLayer(playerLayerType.ToString());
+            }
+            else
+            {
+                playerLayerType = PlayerLayerType.Base;
+                gameObject.layer = LayerMask.NameToLayer(playerLayerType.ToString());
+            }
+
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        
+        if (other.CompareTag("Gate"))
+        {
+
+            if (other.transform.position.z < transform.position.z)
+            {
+                playerLayerType = PlayerLayerType.BattleYard;
+                gameObject.layer = LayerMask.NameToLayer(playerLayerType.ToString());
+            }
+            else
+            {
+                playerLayerType = PlayerLayerType.Base;
+                gameObject.layer = LayerMask.NameToLayer(playerLayerType.ToString());
+
+            }
+
+        }
+
     }
 
 }
